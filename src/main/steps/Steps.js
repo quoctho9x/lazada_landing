b8b7423414f58f69f3ea4cc50c1b23a21f352015
thonq <thonq@vng.com.vn>
2021-03-25 15:46:21 +0700

    update: animation for swiper

--- src/main/steps/Steps.js
+++ src/main/steps/Steps.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from "react";
-import SwiperCore, {Navigation, Pagination, Scrollbar, A11y, Autoplay} from 'swiper';
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade, EffectCube, EffectFlip, EffectCoverflow} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import "./swiperStyle.css";
 import "./styles.css";
 import {stepDetailData, paginationData} from "../../utils/data_mock";
 
 // install Swiper components
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade, EffectCube, EffectFlip, EffectCoverflow]);
 
 const Steps = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -46,17 +46,18 @@ const Steps = () => {
       <Pagination/>
 
       <Swiper
+        effect={'fade'} //'slide' | 'fade' | 'cube' | 'coverflow' | 'flip'
         allowTouchMove={false}
         initialSlide={activeIndex}
         onSwiper={setControlledSwiper}
         // onSlideChange={(swiper) => console.log('onSlideChange',swiper)}
         // pagination={true}
       >
+        <div className={'arrow'} style={{left:`calc(${activeIndex * 25}% + 8%)` }}/>
         {stepDetailData?.length > 0 && stepDetailData.map((item, index) => {
           return (
             <SwiperSlide key={index}>
               <div className={'step-detail'} style={{textAlign: 'center'}}>
-                <div className={'arrow'} style={{left:`calc(${index * 25}% + 8%)` }}/>
                 <div className={'step-detail-title'}>{item.title}</div>
                 <img alt="" className={'step-detail-img'} style={{margin:`${item.margin}`}} src={item.image}/>
                 {item.content && <div className={'step-detail-text'}>{item.content}</div>}

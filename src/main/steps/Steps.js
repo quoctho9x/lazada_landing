dedf738f74bd44637b82591544d4e4fdd813ad42
tho.nguyen3 <tho.nguyen3@tiki.vn>
2021-07-12 14:43:18 +0700

    update dev_2

--- src/main/steps/Steps.js
+++ src/main/steps/Steps.js
@@ -39,7 +39,7 @@ const Steps = () => {
       </div>
     )
   };
-  console.log('day la dev2');
+  console.log('day la dev2 test lai lan nua');
   return (
     <>
     <div className={'steps'} style={{overflow: 'hidden', margin: '20px 16px'}}>
